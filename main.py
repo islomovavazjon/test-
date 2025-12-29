116e92d05e435f728c5c1ca9f13d008253cfd714
Avazjon Islomov <islomovavazjon444@gmail.com>
2025-12-29 16:45:52 +0500

    add some code

--- main.py
+++ main.py
@@ -282,15 +282,26 @@
 
 # constructors - creating new tuples
 
-x = ()
-x = (1, 2, 3)
-x = 1, 2, 3
-x = 2,  # the comma tells Python it's a tuple
-print(x, type(x))
+# x = ()
+# x = (1, 2, 3)
+# x = 1, 2, 3
+# x = 2,  # the comma tells Python it's a tuple
+# print(x, type(x))
 
 
-list1 = [2, 4, 6]
-x = tuple(list1)  
-print(x, type(x))
+# list1 = [2, 4, 6]
+# x = tuple(list1)  
+# print(x, type(x))
 
-# 
+# tuples  are immutable, but member objects may be mutable
+# x = (1, 2, 3)
+# del(x[1]) #fails
+# x[1] = 8 #fails
+# print(x)
+
+y = ([1, 2], 3)
+del(y[0][1])
+print(y)
+
+y += (4, )
+print(y)
