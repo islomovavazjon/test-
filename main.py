3a586b66b5d040b32d128ee40035bfdba83386d1
Avazjon Islomov <islomovavazjon444@gmail.com>
2026-01-01 01:17:48 +0500

    add some new code

--- main.py
+++ main.py
@@ -456,4 +456,54 @@
 # print(letters, letrs)
 
 
+# Stack using Python List
+# Stack is LIFO data structure --last-in, first-out.
+# Use append() to push an item onto the stack.
+# Use pop() to remove an item.
 
+# my_stack = list()
+# my_stack.append(4)
+# my_stack.append(5)
+# my_stack.append(7)
+# my_stack.append(10)
+# print(my_stack)
+
+
+# print(my_stack.pop())
+# print(my_stack.pop())
+
+
+# Stack using List with a Wrapper Class 
+# We create a Stack class and a full set of Stack methods.
+# But the underlying data structure is really a Python List.
+# For pop and peek methods we first check whether the stack is empty, to avoid exceptions.
+
+
+class Stack():
+    def __init__(self):
+        self.stack = list()
+    def push(self, item ):
+        self.stack.append(item) 
+    def pop(self):
+        if len(self.stack) > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def peek(self):
+        if len(self.stack) > 0:
+            return self.stack[len(self.stack) -1 ]
+        else:
+            return None
+    def __str__(self):
+        return str(self.stack)
+
+s = Stack()
+
+s.push(1)
+s.push(2)
+s.push(3)
+
+print(s)
+print(s.pop())
+print(s.peek())
+print(s)
